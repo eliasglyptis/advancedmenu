51c94e9e967156d6d221e2908637eba8750282cc
Elias Glyptis <eliasglyptis@gmail.com>
2022-03-27 18:56:11 +0200

    Completed API for Places

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models, serializers
 
-# Create your views here.
+class PlaceList(generics.ListCreateAPIView):
+  serializer_class = serializers.PlaceSerializer
+
+  def get_query(self):
+    return models.Place.objects.filter(owner_id=self.request.user.id)
+  
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
